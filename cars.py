95404bb80dbbbf3d9362884b170f7155352d5542
pudongqi <pudongqi@gmail.com>
2018-05-16 18:28:30 +0800

    new

--- cars.py
+++ cars.py
@@ -1,24 +1,7 @@
-cars = ['BMW', 'AUDI', 'TOYOTA', 'SUBARU']
+cars = ['audi', 'bmw', 'subaru', 'toyota']
 
-print("Here is the original list:")
-print(cars)
-
-print("\nHere is the sorted list:")
-print(sorted(cars, reverse=True))#倒序
-
-print("\nHere is the original list:")
-print(cars)
-
-#永久排序
-cars.sort()
-print("\n" + str(cars))
-
-#倒着打印,永久的
-cars.reverse()
-print("\n" + str(cars))
-
-
-print(len(cars))
-
-
-
+for car in cars:
+	if car == 'bmw':
+		print(car.upper())
+	else:
+		print(car.title())
--- foods.py
+++ foods.py
@@ -2,6 +2,9 @@
 my_foods = ['pizza', 'falafel', 'carrot cake']
 friend_foods = my_foods[:]
 
+my_foods.append("cannoli")
+friend_foods.append("ice cream")
+
 print("My favorite foods are:")
 print(my_foods)
 
