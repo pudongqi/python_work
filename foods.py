494755d4a71bfe58299de48c6f8bbc90c11dafff
pudongqi <pudongqi@gmail.com>
2018-05-16 17:11:28 +0800

    new

---
+++ foods.py
@@ -0,0 +1,9 @@
+#复制列表
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
--- players.py
+++ players.py
@@ -1,1 +1,19 @@
-players
+#切片
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+print(players[1:4])
+print(players[:4])
+print(players[2:])
+
+for player in players[:3]:
+	print(player.title())
+
+
+values = list(range(1,10000))
+print(values[-3:])
+
+
+lists = [1,2,3,4,5,6,7,8,9]
+lists.sort( reverse = True )
+print(lists[:3])
+
